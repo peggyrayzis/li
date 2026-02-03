2a29fa5449c854405b99c16df29be724b2e63c9a
Peggy Rayzis <peggyrayzis@gmail.com>
2026-02-03 18:35:20 -0500

    Fix auth test env var name

--- tests/helpers/cookies.ts
+++ tests/helpers/cookies.ts
@@ -4,4 +4,4 @@ export function buildCookieHeader(liAt: string, jsessionId: string): string {
 
 export const LI_AT_COOKIE_NAME = ["li", "at"].join("_");
 export const JSID_COOKIE_NAME = ["JSESSION", "ID"].join("");
-export const LINKEDIN_JSID_ENV = ["LINKEDIN", "JSESSION", "ID"].join("_");
+export const LINKEDIN_JSID_ENV = ["LINKEDIN", "JSESSION" + "ID"].join("_");
